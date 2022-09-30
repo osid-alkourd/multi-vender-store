85d4dab40e064d22431b1eabacccc33b4d699c92
Osid ALkourd <alkordosid73@gmail.com>
2022-09-30 11:34:53 -0700

    Add Stores , Categories and Products Table Into Database

---
+++ app/Models/Store.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Store extends Model
+{
+    use HasFactory;
+
+    const CREATED_AT = 'created_at' ;
+    const UPDATED_AT = 'updated_at' ;
+    protected $connection  = 'mysql';
+    protected $table = 'stores' ;
+    protected $primaryKey = 'id' ;
+    public $incrementing = true; // autoincrement
+    public $timestamps = true; //  true: created_at and updated_at field are exist
+}
